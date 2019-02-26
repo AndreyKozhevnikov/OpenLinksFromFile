4e0e1f30f8e018fab10750dad116a464e6161398
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2019-02-26 14:02:00 +0300

    init

---
+++ OpenLinksFromFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenLinksFromFile {
+    class Program {
+        static void Main(string[] args) {
+            var sr = new StreamReader(@"c:\temp\xafissues.txt");
+            var txt = sr.ReadToEnd();
+            var list = txt.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach(var link in list) {
+                if(string.IsNullOrEmpty(link)) {
+                    continue;
+                }
+                System.Diagnostics.Process.Start(link);
+                Thread.Sleep(300);
+            }
+
+        }
+    }
+}
